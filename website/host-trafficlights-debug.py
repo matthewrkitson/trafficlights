7fd8d90a9356e7dcbc844a058fd7f2200ed91846
Matthew Kitson <code@mkitson.co.uk>
2017-08-03 22:20:08 +0000

    Added configuration file for TeamCityUpdater

--- website/host-trafficlights-debug.py
+++ website/host-trafficlights-debug.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 
 from app import app
-app.run(debug=True, host='0.0.0.0')
+app.run(debug=True, host='0.0.0.0', use_reloader=False)
--- website/teamcity_updater.py
+++ website/teamcity_updater.py
@@ -1,6 +1,10 @@
 from controller import Controller
 from urllib.parse import urljoin
 import requests
+import json
+import os
+import base64
+import _thread as thread
 
 class TeamCityUpdater:
     def __init__(self, lights, logger):
@@ -8,15 +12,14 @@ class TeamCityUpdater:
         self.logger = logger
 
     def update(self):
+        print('TeamCity updater running on thread ' + str(thread.get_ident()))
 
-        self.logger.debug('TeamCity updater running')
-
-        build_types = [ 'buildType1' ]
-        hostname = 'http://teamcity/'
-        username = 'user'
-        password = 'password'        
-
-        baseurl = urljoin('http://', hostname)
+        config = TeamCityUpdater._read_config('teamcity-config.json')
+        baseurl = config['baseurl']
+        username = config['username']
+        password = TeamCityUpdater._unobscure(config['obscured_password'])
+        build_types = config['build_types']
+        
         headers = {'Accept': 'application/json'}
 
         for i, build_type in enumerate(build_types):
@@ -44,3 +47,38 @@ class TeamCityUpdater:
             except Exception as ex:
                 self.logger.debug('Error occurred when trying to retrieve build status for ' + build_type)
                 self.logger.exception(ex)
+
+    def _read_config(config_path):
+        if not os.path.isfile(config_path):
+            TeamCityUpdater._write_default_config(config_path)
+            
+        with open(config_path) as config_stream:
+            config = json.load(config_stream)
+
+        return config
+        
+    def _write_config(config, config_path):
+        with open(config_path, 'w') as config_stream:
+            json.dump(config, config_stream, indent=4)
+
+    def _write_default_config(config_path):
+        config = { 
+            'baseurl': 'http://teamcity/',
+            'username': 'username',
+            'obscured_password': TeamCityUpdater._obscure('password'),
+            'build_types': [
+                'buildType1'
+            ]
+        }
+        
+        TeamCityUpdater._write_config(config, config_path)
+
+    def _obscure(plaintext):
+        # This does nothing more than prevent casual observtion
+        # of the password. However, even if proper encryption
+        # was used, we'd still have to store the password somewhere
+        # so it wouldn't actually offer any extra security. 
+        return base64.b64encode(plaintext.encode('utf-8')).decode('ascii')
+        
+    def _unobscure(cyphertext):
+        return base64.b64decode(cyphertext).decode('utf-8')
