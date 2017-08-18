ed185c1c27dd3f8e521ad5c9de56cf372cdb2dc6
Matthew Kitson <code@mkitson.co.uk>
2017-08-18 17:12:40 +0000

    Updated index.html to POST data to server

--- website/trafficlights/views/index.py
+++ website/trafficlights/views/index.py
@@ -2,14 +2,18 @@ from trafficlights import app, lights
 from flask import render_template, request
 import trafficlights.controller as controller
 
-@app.route('/')
-def index():
+@app.route('/', methods=['GET'])
+def index_get(errors = []):    
+    return render_template('index.html', lights=lights, errors=errors)
+    
+@app.route('/', methods=['POST'])
+def index_post():
     errors = []
     
     for i in range(lights.num_indicators):
         arg = 'pair' + str(i)
-        if arg in request.args:
-            value = request.args[arg]
+        if arg in request.form:
+            value = request.form[arg]
             if value == 'red':
                 lights.set_indicator(i, controller.Controller.RED)
             elif value == 'green':
@@ -23,8 +27,8 @@ def index():
                 
     for i in range(lights.num_buzzers):
         arg = 'buzzer' + str(i)
-        if arg in request.args:
-            value = request.args[arg]
+        if arg in request.form:
+            value = request.form[arg]
 
             try:
                 duration = int(value)
@@ -32,5 +36,7 @@ def index():
                 errors.append('Unable to convert ' + value + ' to ms duration for ' + arg + ': ' + str(ex))
 
             lights.buzz(i, duration)
+    
+    return index_get(errors)
 
-    return render_template('index.html', lights=lights, errors=errors)
+
