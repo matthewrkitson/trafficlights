59c465b88695e24e6e343e96872c5f56a7fdd3df
Matthew Kitson <matthew.kitson@grantadesign.com>
2018-03-02 13:42:47 +0000

    Added single call implementation

--- meetingrooms/meetingrooms.py
+++ meetingrooms/meetingrooms.py
@@ -9,18 +9,18 @@ from gpiozero import LED
 leds = [10, 22, 27, 17, 4, 3, 2, 14, 15, 18, 23, 24, 25, 8, 7, 1, 12, 16, 20, 21, 26, 19, 13, 6]
 
 rooms = { 
-	'u1': (LED(leds[0]), LED(leds[1])),
-	'u2': (LED(leds[2]), LED(leds[3])),
-	'u3': (LED(leds[4]), LED(leds[5])),
-	'u4': (LED(leds[6]), LED(leds[7])),
-	'u5': (LED(leds[8]), LED(leds[9])),
-	'u6': (LED(leds[10]), LED(leds[11])),
-	'd1': (LED(leds[12]), LED(leds[13])),
-	'd2': (LED(leds[14]), LED(leds[15])),
-	'd3': (LED(leds[16]), LED(leds[17])),
-	'd4': (LED(leds[18]), LED(leds[19])),
-	'd5': (LED(leds[20]), LED(leds[21])),
-	'd6': (LED(leds[22]), LED(leds[23]))
+	'U1': (LED(leds[0]), LED(leds[1])),
+	'U2': (LED(leds[2]), LED(leds[3])),
+	'U3': (LED(leds[4]), LED(leds[5])),
+	'U4': (LED(leds[6]), LED(leds[7])),
+	'U5': (LED(leds[8]), LED(leds[9])),
+	'U6': (LED(leds[10]), LED(leds[11])),
+	'D1': (LED(leds[12]), LED(leds[13])),
+	'D2': (LED(leds[14]), LED(leds[15])),
+	'D3': (LED(leds[16]), LED(leds[17])),
+	'D4': (LED(leds[18]), LED(leds[19])),
+	'D5': (LED(leds[20]), LED(leds[21])),
+	'D6': (LED(leds[22]), LED(leds[23]))
 }
 
 def green(pair):
@@ -39,17 +39,34 @@ def off(pair):
 	pair[0].off()
 	pair[1].off()
 
-while True:
-	for room in rooms:
-		contents = urllib.request.urlopen("http://localhost:9000/api/meetingroom/" + room)
-		jsontext = contents.read()
-		print(jsontext)
-		result = json.loads(jsontext.decode("utf8"))
+def update_with_single_call():
+	contents = urllib.request.urlopen("http://localhost:9000/api/meetingroom")
+	jsontext = contents.read()
+	results = json.loads(jsontext.decode("utf-8"))
+	for result in results:
+		room = result["room"]
 		busy = result["busy"]
 		print(room + " " + str(busy))
 		if busy:
 			red(rooms[room])
 		else:
 			green(rooms[room])
+	time.sleep(1)
+
+def update_with_multiple_calls():
+	contents = urllib.request.urlopen("http://localhost:9000/api/meetingroom/" + room)
+	jsontext = contents.read()
+	print(jsontext)
+	result = json.loads(jsontext.decode("utf8"))
+	busy = result["busy"]
+	print(room + " " + str(busy))
+	if busy:
+		red(rooms[room])
+	else:
+		green(rooms[room])
 
 
+while True:
+	for room in rooms:
+		update_with_single_call()
+		# update_with_multiple_calls()
