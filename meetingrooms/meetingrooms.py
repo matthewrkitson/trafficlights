a502be4bb03d9fae684cc8d86d57e7afe5958421
Matthew Kitson <matthewrkitson@users.noreply.github.com>
2018-03-19 07:51:54 +0000

    Added support for poweroff button

--- meetingrooms/meetingrooms.py
+++ meetingrooms/meetingrooms.py
@@ -3,74 +3,93 @@
 import time
 import urllib.request
 import json
-from gpiozero import LED
+from gpiozero import LED, Button
+from subprocess import check_call
 
 # LED pinout for floorplan unit
 leds = [10, 22, 27, 17, 4, 3, 2, 14, 15, 18, 23, 24, 25, 8, 7, 1, 12, 16, 20, 21, 26, 19, 13, 6]
 
-rooms = { 
-	'U1': (LED(leds[0]), LED(leds[1])),
-	'U2': (LED(leds[2]), LED(leds[3])),
-	'U3': (LED(leds[4]), LED(leds[5])),
-	'U4': (LED(leds[6]), LED(leds[7])),
-	'U5': (LED(leds[8]), LED(leds[9])),
-	'U6': (LED(leds[10]), LED(leds[11])),
-	'D1': (LED(leds[12]), LED(leds[13])),
-	'D2': (LED(leds[14]), LED(leds[15])),
-	'D3': (LED(leds[16]), LED(leds[17])),
-	'D4': (LED(leds[18]), LED(leds[19])),
-	'D5': (LED(leds[20]), LED(leds[21])),
-	'D6': (LED(leds[22]), LED(leds[23]))
+rooms = {
+        'U1': (LED(leds[0]), LED(leds[1])),
+        'U2': (LED(leds[2]), LED(leds[3])),
+        'U3': (LED(leds[4]), LED(leds[5])),
+        'U4': (LED(leds[6]), LED(leds[7])),
+        'U5': (LED(leds[8]), LED(leds[9])),
+        'U6': (LED(leds[10]), LED(leds[11])),
+        'D1': (LED(leds[12]), LED(leds[13])),
+        'D2': (LED(leds[14]), LED(leds[15])),
+        'D3': (LED(leds[16]), LED(leds[17])),
+        'D4': (LED(leds[18]), LED(leds[19])),
+        'D5': (LED(leds[20]), LED(leds[21])),
+        'D6': (LED(leds[22]), LED(leds[23]))
 }
 
+def poweroff():
+        all_on()
+        check_call(['sudo', 'poweroff'])
+
+def reboot():
+        all_on()
+        check_call(['sudo', 'reboot'])
+
+button = Button(5, hold_time=2)
+button.when_held = poweroff
+button.when_released = reboot
+
 def green(pair):
-	pair[0].off()
-	pair[1].on()
+        pair[1].off()
+        pair[0].on()
 
 def red(pair):
-	pair[0].on()
-	pair[1].off()
+        pair[1].on()
+        pair[0].off()
 
-def orange(pair):
-	pair[0].on()
-	pair[1].on()
+def on(pair):
+        pair[0].on()
+        pair[1].on()
 
 def off(pair):
-	pair[0].off()
-	pair[1].off()
+        pair[0].off()
+        pair[1].off()
+
+def all_on():
+        for room in rooms:
+                on(rooms[room])
 
 def update_with_single_call():
-	contents = urllib.request.urlopen("http://localhost:9000/api/meetingroom")
-	jsontext = contents.read()
-	results = json.loads(jsontext.decode("utf-8"))
-	for result in results:
-		room = result["room"]
-		busy = result["busy"]
-		print(room + " " + str(busy))
-		if busy:
-			red(rooms[room])
-		else:
-			green(rooms[room])
-	time.sleep(1)
+        contents = urllib.request.urlopen("http://meeting-server:9000/api/meetingroom")
+        jsontext = contents.read()
+        results = json.loads(jsontext.decode("utf-8"))
+        for result in results:
+                room = result["room"]
+                busy = result["busy"]
+                print(room + " " + str(busy))
+                if busy:
+                        red(rooms[room])
+                else:
+                        green(rooms[room])
 
 def update_with_multiple_calls():
-	contents = urllib.request.urlopen("http://localhost:9000/api/meetingroom/" + room)
-	jsontext = contents.read()
-	print(jsontext)
-	result = json.loads(jsontext.decode("utf8"))
-	busy = result["busy"]
-	print(room + " " + str(busy))
-	if busy:
-		red(rooms[room])
-	else:
-		green(rooms[room])
+        contents = urllib.request.urlopen("http://localhost:9000/api/meetingroom/" + room)
+        jsontext = contents.read()
+        print(jsontext)
+        result = json.loads(jsontext.decode("utf8"))
+        busy = result["busy"]
+        print(room + " " + str(busy))
+        if busy:
+                red(rooms[room])
+        else:
+                green(rooms[room])
 
 
 while True:
-	try:
-		update_with_single_call()
+        try:
+                update_with_single_call()
 
-		# for room in rooms:
-			# update_with_multiple_calls()
-	except Exception as e:
-		print(e)
+                # for room in rooms:
+                        # update_with_multiple_calls()
+        except Exception as e:
+                print(e)
+
+        time.sleep(5)
+
