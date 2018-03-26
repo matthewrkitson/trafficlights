bcb5a07e7dcfd608116210e9ae82f7b4dca02971
Matthew Kitson <matthewrkitson@users.noreply.github.com>
2018-03-26 12:16:10 +0100

    Added alternative pinout for floorplan unit

--- meetingrooms/meetingrooms.py
+++ meetingrooms/meetingrooms.py
@@ -8,8 +8,16 @@ import json
 from gpiozero import LED, Button
 from subprocess import check_call
 
-# LED pinout for floorplan unit
-leds = [10, 22, 27, 17, 4, 3, 2, 14, 15, 18, 23, 24, 25, 8, 7, 1, 12, 16, 20, 21, 26, 19, 13, 6]
+# LED pinout for "black box" units
+# Top row, green, red, green, red, then bottom row green, red, green, red, etc.
+black_box_leds = [10, 22, 27, 17, 4, 3, 2, 14, 15, 18, 23, 24, 25, 8, 7, 1, 12, 16, 20, 21, 26, 19, 13, 6]
+
+# LED pinout for "floorplan" unit
+# U1 red, green, U2 red green, etc... then D1 red, green, D2 red, green, etc.
+floorplan_leds = [22, 10, 17, 27, 3, 4, 14, 2, 18, 15, 24, 23, 8, 25, 1, 7, 16, 12, 21, 20, 19, 26, 6, 13]
+
+# Choose the appropriate pinout here. 
+leds = black_box_leds
 
 rooms = {
         'U1': (LED(leds[0]), LED(leds[1])),
