f996209d0e28d2fe9676ebab31aef0a0b1c6582c
Matthew Kitson <matthew.kitson@grantadesign.com>
2018-04-13 19:11:57 +0000

    Added console app to display light status

--- meetingrooms/lights.py
+++ meetingrooms/lights.py
@@ -1,3 +1,50 @@
+import json
+from gpiozero import LED
+
+def get_config():
+    # Read the configuration file (pinout.config)
+    with open('meetingrooms.config') as json_file:
+        config = json.load(json_file)
+
+    return config
+
+def get_pinout():
+    # Dictionary of pinouts of different hardware units. 
+    # Order of pins is: U1 green, U1 red, U2 green, U2 red, ... U6 green, U6 red, D1 green, D1 red, etc...
+    pinouts = {
+        'blackbox': [10, 22, 27, 17, 4, 3, 2, 14, 15, 18, 23, 24, 25, 8, 7, 1, 12, 16, 20, 21, 26, 19, 13, 6],
+        'floorplan': [22, 10, 17, 27, 3, 4, 14, 2, 18, 15, 24, 23, 8, 25, 1, 7, 16, 12, 21, 20, 19, 26, 6, 13]
+    }
+
+    config = get_config()
+    return pinouts[config['pinout']]
+
+def get_lights(initialise = True):
+    leds = get_pinout()
+
+    if (initialise):
+        initial_value = False
+    else:
+        initial_value = None
+
+    # A dictionary of light pairs. Key is row.column
+    lights = {
+        '0.0': (LED(leds[0], initial_value=initial_value), LED(leds[1], initial_value=initial_value)),
+        '0.1': (LED(leds[2], initial_value=initial_value), LED(leds[3], initial_value=initial_value)),
+        '0.2': (LED(leds[4], initial_value=initial_value), LED(leds[5], initial_value=initial_value)),
+        '0.3': (LED(leds[6], initial_value=initial_value), LED(leds[7], initial_value=initial_value)),
+        '0.4': (LED(leds[8], initial_value=initial_value), LED(leds[9], initial_value=initial_value)),
+        '0.5': (LED(leds[10], initial_value=initial_value), LED(leds[11], initial_value=initial_value)),
+        '1.0': (LED(leds[12], initial_value=initial_value), LED(leds[13], initial_value=initial_value)),
+        '1.1': (LED(leds[14], initial_value=initial_value), LED(leds[15], initial_value=initial_value)),
+        '1.2': (LED(leds[16], initial_value=initial_value), LED(leds[17], initial_value=initial_value)),
+        '1.3': (LED(leds[18], initial_value=initial_value), LED(leds[19], initial_value=initial_value)),
+        '1.4': (LED(leds[20], initial_value=initial_value), LED(leds[21], initial_value=initial_value)),
+        '1.5': (LED(leds[22], initial_value=initial_value), LED(leds[23], initial_value=initial_value))
+    }
+
+    return lights
+
 def green(pair):
         pair[0].on()
         pair[1].off()
--- meetingrooms/meetingrooms.py
+++ meetingrooms/meetingrooms.py
@@ -1,49 +1,13 @@
 #!/usr/bin/python3
 
 import time
-import urllib.request
 import json
-from gpiozero import LED, Button
+import urllib.request
+from gpiozero import Button
 from subprocess import check_call
 
 from log import logger
-from lights import red, green, on, off, all_on
-
-def get_config():
-    # Read the configuration file (pinout.config)
-    with open('meetingrooms.config') as json_file:
-        config = json.load(json_file)
-
-    return config
-
-def get_pinout():
-    # Dictionary of pinouts of different hardware units. 
-    # Order of pins is: U1 green, U1 red, U2 green, U2 red, ... U6 green, U6 red, D1 green, D1 red, etc...
-    pinouts = {
-        'blackbox': [10, 22, 27, 17, 4, 3, 2, 14, 15, 18, 23, 24, 25, 8, 7, 1, 12, 16, 20, 21, 26, 19, 13, 6],
-        'floorplan': [22, 10, 17, 27, 3, 4, 14, 2, 18, 15, 24, 23, 8, 25, 1, 7, 16, 12, 21, 20, 19, 26, 6, 13]
-    }
-
-    config = get_config()
-    return pinouts[config['pinout']]
-
-# Choose the appropriate pinout here. 
-leds = get_pinout()
-
-rooms = {
-        'U1': (LED(leds[0]), LED(leds[1])),
-        'U2': (LED(leds[2]), LED(leds[3])),
-        'U3': (LED(leds[4]), LED(leds[5])),
-        'U4': (LED(leds[6]), LED(leds[7])),
-        'U5': (LED(leds[8]), LED(leds[9])),
-        'U6': (LED(leds[10]), LED(leds[11])),
-        'D1': (LED(leds[12]), LED(leds[13])),
-        'D2': (LED(leds[14]), LED(leds[15])),
-        'D3': (LED(leds[16]), LED(leds[17])),
-        'D4': (LED(leds[18]), LED(leds[19])),
-        'D5': (LED(leds[20]), LED(leds[21])),
-        'D6': (LED(leds[22]), LED(leds[23]))
-}
+from lights import get_lights, red, green, on, off, all_on
 
 def poweroff():
         all_on()
@@ -53,17 +17,13 @@ def reboot():
         all_on()
         check_call(['sudo', 'reboot'])
 
-button = Button(5, hold_time=2)
-button.when_held = poweroff
-button.when_released = reboot
-
 def format_room(room, busy):
     if busy:
         return "(*) " + room + "   "
     else:
         return "( ) " + room + "   "
 
-def update_with_single_call():
+def update_with_single_call(roomlights):
     with urllib.request.urlopen("http://meeting-server:9000/api/meetingroom", timeout=20) as contents:
             jsontext = contents.read()
             results = json.loads(jsontext.decode("utf-8"))
@@ -73,15 +33,36 @@ def update_with_single_call():
                     busy = result["busy"]
                     room_statuses += format_room(room, busy)
                     if busy:
-                            red(rooms[room])
+                            red(roomlights[room])
                     else:
-                            green(rooms[room])
+                            green(roomlights[room])
             logger.info(room_statuses)
 
-               
+
+
+button = Button(5, hold_time=2)
+button.when_held = poweroff
+button.when_released = reboot
+
+lights = get_lights()
+roomlights = {
+    'U1': lights['0.0'],
+    'U2': lights['0.1'],          
+    'U3': lights['0.2'],
+    'U4': lights['0.3'],
+    'U5': lights['0.4'],
+    'U6': lights['0.5'],
+    'D1': lights['1.0'],
+    'D2': lights['1.1'],
+    'D3': lights['1.2'],
+    'D4': lights['1.3'],
+    'D5': lights['1.4'],
+    'D6': lights['1.5']
+}
+
 while True:
     try:
-        update_with_single_call()
+        update_with_single_call(roomlights)
 
     except Exception as e:
         logger.error(str(e))
